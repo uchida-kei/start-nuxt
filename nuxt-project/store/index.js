da83cebd9544ea1bf7d19afb0d00480ced120acd
k15024 <k15024kk@aitech.ac.jp>
2018-10-01 07:18:41 +0900

    add store

---
+++ nuxt-project/store/index.js
@@ -0,0 +1,27 @@
+import Vuex from 'vuex'
+const store = () => new Vuex.Store({
+    store: {
+        user: null
+    },
+    getters: {
+        user: (state) => state.user
+    },
+    mutations: {
+        saveUser (state, { user }) {
+            state.user = user
+        }
+    },
+    actions: {
+        async getUser ({ commit }, { id }) {
+            try {
+                const user = await this.$axios.$get(
+                    'https://api.github.com/users/'+id
+                )
+                commit('saveUser', { user })
+            } catch (e) {
+                return Promise.reject(e)
+            }
+        }
+    }
+})
+export default store
